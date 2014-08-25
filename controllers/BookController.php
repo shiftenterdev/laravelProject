3f7875682dddd20fb65240f1003ef7d88c9b9d7d
Bappa <iftakharul.alam@live.com>
2014-08-25 12:44:28 -0800

    book controller added

---
+++ controllers/BookController.php
@@ -0,0 +1,75 @@
+<?php
+
+class BookController extends \BaseController {
+
+
+    public function index()
+    {
+        $booksList=Book::all();
+        return View::make('books.index',compact('booksList'));
+    }
+
+
+
+    public function create()
+    {
+        //
+    }
+
+
+
+    public function store()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+
+}
